09b925c1d49aee7feb2e58d6271566df9d7d2665
gil <gsqsanchez@gmail.com>
2024-11-25 00:03:19 -0700

    Add multer

---
+++ src/config/multer.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+})
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, 
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png|gif/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = fileTypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb(new Error('Only images are allowed!'));
+    }
+  },
+});
--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import isAuthenticated from '../middleware/authMiddleware';
-import { createOrder, deleteOrder, getAllOrders, getOrderById, updateOrder } from '../controllers/orderController';
+import { createOrder, deleteOrder, getAllOrders, getOrderById, updateOrder, uploadOrderImage } from '../controllers/orderController';
+import { upload } from '../config/multer';
 const router = Router();
 
 router.get('/:customerNumber/:id', getOrderById);
 router.get('/', isAuthenticated, getAllOrders);
 router.post('/', isAuthenticated, createOrder);
+router.post('/upload/:id', upload.single('image'), uploadOrderImage); 
 router.put('/:id', isAuthenticated, updateOrder);
 router.delete('/:id', isAuthenticated, deleteOrder);
 
